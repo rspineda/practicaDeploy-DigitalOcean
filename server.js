164231ddeeaef9d2fa42664c1aeed85d428ab66e
Ronald <ronystivens@hotmail.com>
2020-07-23 22:56:30 +0200

    balanceo de carga con nginx

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const path = require('path')
+const argv = require('optimist').argv
 const app = express()
 
 app.get('/', (req, res)=>{
+    console.log('Respondiendo en el puerto: ', argv.port)
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 app.listen(3000, ()=>{
-    console.log('server on port 3000')
+    console.log('server on port ', argv.port)
 })
